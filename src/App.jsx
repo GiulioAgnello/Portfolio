e3307f2f20db3cbd3bf768a788dfc2126232cdd9
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-11-29 19:49:04 +0100

    scheletro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Homepage from "./pages/Homepage";
+import DefaultLayout from "./layout/DefaultLayout";
+
 export default function App() {
   return (
-    <>
-      <h1>giulio</h1>;
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Homepage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
