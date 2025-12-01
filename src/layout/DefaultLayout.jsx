debf12a5de74c4cdcc279a7451a4cf462b76116d
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-12-01 12:50:56 +0100

    backgronudLiquid

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,11 +1,39 @@
 import { Outlet } from "react-router-dom";
-
+import LiquidEther from "../components/LiquidEther";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function DefaultLayout() {
   return (
     <>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      >
+        <LiquidEther
+          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
       <Header />
       <main>
         <Outlet />
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,21 +4,48 @@ export default function Homepage() {
   return (
     <>
       <div className="wrapperHome vh-100">
-        <div className="container h-50 d-flex justify-content-center align-items-center">
-          <div className="d-flex gap-2 justify-content-center align-items-center fs-1 sm:fs-4 md:fs-5">
-            <p className="m-0">Giulio</p>
-            <RotatingText
-              texts={["Agnello", "Developer", "Design", "React", "JavaScript"]}
-              mainClassName="divRoll px-2 sm:px-2 md:px-3  text-light overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg d-flex align-items-center"
-              staggerFrom={"last"}
-              initial={{ y: "100%" }}
-              animate={{ y: 0 }}
-              exit={{ y: "-120%" }}
-              staggerDuration={0.025}
-              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-              transition={{ type: "spring", damping: 30, stiffness: 400 }}
-              rotationInterval={2000}
-            />
+        <div className="container ">
+          <div className="row py-5 d-flex justify-content-between align-items-center">
+            <div className="col-3">
+              <div className="fotoHome"></div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-column gap-3 justify-content-center align-items-center">
+                <div className="d-flex gap-2 justify-content-center align-items-center fs-1 sm:fs-4 md:fs-5">
+                  <p className="m-0 fw-bold text-light">Giulio</p>
+                  <RotatingText
+                    texts={[
+                      "Agnello",
+                      "Developer",
+                      "Design",
+                      "React",
+                      "JavaScript",
+                    ]}
+                    mainClassName="divRoll px-2 sm:px-2 md:px-3  text-light overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg d-flex align-items-center"
+                    staggerFrom={"last"}
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    exit={{ y: "-120%" }}
+                    staggerDuration={0.025}
+                    splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                    transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                    rotationInterval={2000}
+                  />
+                </div>
+                <div className="descriptionHome">
+                  <p>
+                    💻| FullStack Dev | Front-End Developer | 🎨 UI/UX
+                    Enthusiast | 🚀 Curious Mind Passionate about building
+                    clean, intuitive and elegant digital experiences. Amo
+                    progettare interfacce che parlano da sole, combinando
+                    design, logica e performance. Dopo una laurea in Industrial
+                    Design, ho ampliato le mie competenze nel mondo del web
+                    development con Boolean, creando progetti sempre più
+                    complessi e connessi.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
